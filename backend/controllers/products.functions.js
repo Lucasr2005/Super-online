7b3bbde90a5102a770f13cd51f0e43c9c8e3f0ef
Lucas Romero <lukisromero@hotmail.com.ar>
2025-08-16 15:28:25 -0300

    Implement get product by id functionality

--- backend/controllers/products.functions.js
+++ backend/controllers/products.functions.js
@@ -27,10 +27,30 @@ export async function getProducts(req, res) {
             return res.status(404).json({ message: "No se han encontrado productos" })
         }
         const products = result.rows
-        return res.status(200).json({ products })
+        return res.status(200).json(products)
 
     } catch (error) {
         return res.status(500).json({ message: "Ha ocurrido un error al buscar los productos" })
 
     }
-} 
+}
+
+export async function getProductById(req, res) {
+    const { id } = req.params
+    if (!id) {
+        return res.status(400).json({ message: "Datos inclompletos" })
+    }
+    try {
+        const result = await pool.query("SELECT * FROM products WHERE id=$1", [id])
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: "No se ha encontrado el producto" })
+        }
+        const product = result.rows[0]
+        return res.status(200).json(product)
+
+    } catch (error) {
+        return res.status(500).json({ message: "Ha ocurrido un error al buscar el producto" })
+
+    }
+}
+
--- backend/routes/products.routes.js
+++ backend/routes/products.routes.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { createProduct, getProducts } from "../controllers/products.functions.js"
+import { createProduct, getProducts, getProductById } from "../controllers/products.functions.js"
 const router = express.Router()
 
 router.post("/products", createProduct)
 router.get("/products", getProducts)
+router.get("/products/:id", getProductById)
 
 export default router

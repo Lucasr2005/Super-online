994779c7c8ff2d5939f0614f105abca78ab273da
Lucas Romero <lukisromero@hotmail.com.ar>
2025-08-16 15:23:32 -0300

    Implement get all products functionality

--- backend/routes/products.routes.js
+++ backend/routes/products.routes.js
@@ -1,7 +1,8 @@
 import express from "express"
-import { createProduct } from "../controllers/products.functions.js"
+import { createProduct, getProducts } from "../controllers/products.functions.js"
 const router = express.Router()
 
 router.post("/products", createProduct)
+router.get("/products", getProducts)
 
 export default router

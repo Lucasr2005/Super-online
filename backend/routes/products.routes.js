0fc26e15031abeb5b532cb9f7f5a279b0a0e0d2c
Lucas Romero <lukisromero@hotmail.com.ar>
2025-08-16 15:38:23 -0300

    Implement update product by id functionality

--- backend/routes/products.routes.js
+++ backend/routes/products.routes.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { createProduct, getProducts, getProductById, deleteProductById } from "../controllers/products.functions.js"
+import { createProduct, getProducts, getProductById, deleteProductById, updateProductById } from "../controllers/products.functions.js"
 const router = express.Router()
 
 router.post("/products", createProduct)
 router.get("/products", getProducts)
 router.get("/products/:id", getProductById)
+router.put("/products/:id", updateProductById)
 router.delete("/products/:id", deleteProductById)
 
 export default router

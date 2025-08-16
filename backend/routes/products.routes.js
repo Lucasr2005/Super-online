893d1ae11cee7fffb720e4250b3e80eeee529d85
Lucas Romero <lukisromero@hotmail.com.ar>
2025-08-16 15:19:28 -0300

    Implement create product functionality

--- backend/routes/products.routes.js
+++ backend/routes/products.routes.js
@@ -1,9 +1,7 @@
 import express from "express"
-
+import { createProduct } from "../controllers/products.functions.js"
 const router = express.Router()
 
-router.get("/products", (req, res) => {
-    res.send("Get products")
-})
+router.post("/products", createProduct)
 
 export default router

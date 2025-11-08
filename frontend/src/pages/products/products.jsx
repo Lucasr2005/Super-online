047d4cf7bb02aa2d54bbd22eff146246632c64ac
Lucas Romero <lukisromero@hotmail.com.ar>
2025-11-08 14:26:47 -0300

    improve product card styles

--- frontend/src/pages/products/products.jsx
+++ frontend/src/pages/products/products.jsx
@@ -12,17 +12,19 @@ function Products() {
           {products.map((product) => (
             <div
               key={product.id}
-              className=" bg-white p-3 rounded-md flex flex-col items-center  border-solid border-gray-400 border-1"
+              className=" bg-[#FFFFFF] p-3 rounded-md flex flex-col items-center  shadow-[0px_4px_8px_0px_rgba(0,_0,_0,_0.1)]"
             >
               <img
                 src={product.image_url}
                 alt=""
                 className="w-40 mb-5 rounded-md "
               />
-              <h2 className="w-full  ">{product.name}</h2>
+              <h2 className="w-full text-balance font-semibold opacity-75 line-clamp-2 mb-1">
+                {product.name}
+              </h2>
               <div className="mt-auto w-full">
-                <p className="w-full">{product.price}</p>
-                <button className="bg-gray-900 text-white p-2 w-full text-sm">
+                <p className="w-full">${product.price}</p>
+                <button className="bg-[#007BFF] text-white p-2 my-3 w-full text-sm rounded-sm cursor-pointer">
                   Agregar al carrito
                 </button>
               </div>

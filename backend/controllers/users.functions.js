b13767a19045f421ed0081ab37476b3da8ca46f9
Lucas Romero <lukisromero@hotmail.com.ar>
2025-12-30 15:53:46 -0300

    Enhance registration security with bcrypt and validations

--- backend/controllers/users.functions.js
+++ backend/controllers/users.functions.js
@@ -1,26 +1,32 @@
 import pool from "../database/db.connection.js"
-import passwordHash from "password-hash"
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export async function register(req, res) {
     const { name, lastName, email, password } = req.body
 
     if (!name || !lastName || !email || !password) {
-        return res.status(400).send({ message: "Datos incompletos" })
+        return res.status(400).json({ message: "Datos incompletos" })
+    }
+
+    if (password.length < 8) {
+        return res.status(400).json({ message: "La contraseña debe tener al menos 8 caracteres." });
     }
 
     try {
         const findEmail = await pool.query("SELECT email FROM users WHERE email =$1", [email])
         if (findEmail.rows.length > 0) {
-            return res.status(400).send({ message: "Email ya existente" })
+
+            return res.status(409).json({ message: "El email ya se encuentra registrado." })
         }
 
-        const hashedPassword = passwordHash.generate(password)
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         const result = await pool.query("INSERT INTO users (name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING id", [name, lastName, email, hashedPassword])
 
         if (result.rows.length === 0) {
-            return res.status(500).send({ message: "Ha ocurrido un error al crear el usuario" })
+            return res.status(500).json({ message: "Ha ocurrido un error al crear el usuario" })
         }
 
         const id = result.rows[0].id

b162451d93ef88c5719ab1a13bc34c060e13c1eb
Lucas Romero <lukisromero@hotmail.com.ar>
2025-12-31 08:38:50 -0300

    Enable CORS credentials for secure cookie handling

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import ordersRouter from "./routes/orders.routes.js"
 configDotenv({ quiet: true })
 const app = express()
 
-app.use(cors())
+app.use(cors({ credentials: true, origin: [process.env.CLIENT_URL] }))
 app.use(express.json())
 app.use("/api", productsRouter)
 app.use("/api", usersRouter)

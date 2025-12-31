0920ebf397abcd49be3320ed9e7352a0e8d60201
Lucas Romero <lukisromero@hotmail.com.ar>
2025-12-31 08:25:26 -0300

    Implement login form submission

--- frontend/src/pages/register/login.jsx
+++ frontend/src/pages/register/login.jsx
@@ -1,6 +1,20 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { InputField } from "./components/inputField.jsx";
+import { loginUser } from "../../services/users.js";
+
+const handleSubmit = (e, formData, setError) => {
+  e.preventDefault();
+
+  loginUser(formData)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      setError(err);
+      setTimeout(() => setError(null), 5000);
+    });
+};
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -8,6 +22,7 @@ function Login() {
     password: "",
   });
   const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -19,8 +34,12 @@ function Login() {
 
   return (
     <section className="flex justify-center items-center h-screen">
-      <form className="flex flex-col w-80 gap-5 px-3">
+      <form
+        className="flex flex-col w-80 gap-5 px-3"
+        onSubmit={(e) => handleSubmit(e, formData, setError)}
+      >
         <h2 className="text-2xl font-semibold">Iniciar sesión</h2>
+        <p className="text-red-600 min-h-[1.5rem]">{error ? error.message : ""}</p>
 
         <InputField
           label="Email"
--- frontend/src/services/users.js
+++ frontend/src/services/users.js
@@ -1,13 +1,23 @@
 import axios from "axios";
 
-const URL_BASE = "http://localhost:3000/api/register";
+const URL_BASE = "http://localhost:3000/api";
 
 export const registerUser = async (user) => {
     try {
-        const response = await axios.post(URL_BASE, user);
+        const response = await axios.post(`${URL_BASE}/register`, user, { withCredentials: true });
         return response.data;
     } catch (error) {
         console.error(error);
         throw new Error(error.response?.data?.message || "Ocurrió un error al registrar el usuario.");
+    }
+}
+
+export const loginUser = async (user) => {
+    try {
+        const response = await axios.post(`${URL_BASE}/login`, user, { withCredentials: true });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw new Error(error.response?.data?.message || "Ocurrió un error al logear el usuario.");
     }
 }

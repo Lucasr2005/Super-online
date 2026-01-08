0db2597a2bb3b024153372b5417393eeeea776c5
Lucas Romero <lukisromero@hotmail.com.ar>
2026-01-08 10:50:51 -0300

    Add hero banner to home page

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,7 +1,9 @@
 import { Categories } from "./components/categories.jsx";
+import { Hero } from "./components/hero.jsx";
 function Home() {
   return (
     <>
+      <Hero />
       <Categories />
     </>
   );

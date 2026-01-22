693474d46f4a3fa3b957ad4012b863a80a54c939
Lucas Romero <lukisromero@hotmail.com.ar>
2026-01-22 20:26:30 -0300

    Display order items in checkout summary view

--- frontend/src/pages/checkout/checkout.jsx
+++ frontend/src/pages/checkout/checkout.jsx
@@ -1,5 +1,38 @@
+import { useSelector } from "react-redux";
+import { Product } from "./components/product.jsx";
+
 function Checkout() {
-  return <div>Checkout</div>;
+  const products = useSelector((state) => state.products);
+  const cart = useSelector((state) => state.cart);
+
+  return (
+    <>
+      <h2 className=" text-2xl font-semibold my-5 w-full text-center">Finalizar compra</h2>
+      <section className="bg-[#FFFFFF] rounded-md shadow-[0px_4px_8px_0px_rgba(0,_0,_0,_0.1)] mx-5 py-5">
+        <h2 className=" text-xl font-medium px-3 mb-10">Resumen del pedido</h2>
+        <section className="flex flex-col gap-2">
+          {cart &&
+            products &&
+            cart.map((p) => {
+              const product = products.find((product) => product.id === p.id);
+              return (
+                product && (
+                  <Product
+                    key={p.id}
+                    id={p.id}
+                    image_url={product.image_url}
+                    name={product.name}
+                    price={product.price}
+                    quantity={p.quantity}
+                  />
+                )
+              );
+            })}
+        </section>
+        <hr className="mx-5 opacity-50 my-4" />
+      </section>
+    </>
+  );
 }
 
 export default Checkout;
---
+++ frontend/src/pages/checkout/components/product.jsx
@@ -0,0 +1,11 @@
+export function Product({ id, image_url, name, price, quantity }) {
+  return (
+    <>
+      <article className="flex flex-row justify-between px-5 items-center text-sm ">
+        <span className="font-semibold w-6 text-center">{quantity}</span>
+        <h2 className="text-balance opacity-75 line-clamp-1 max-w-[200px] w-[200px]">{name}</h2>
+        <p className="font-semibold w-12">${price}</p>
+      </article>
+    </>
+  );
+}

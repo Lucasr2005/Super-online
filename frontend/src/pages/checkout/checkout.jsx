7a81009a87e2cb654df61ab6011a15d363ec535b
Lucas Romero <lukisromero@hotmail.com.ar>
2026-01-26 23:52:20 -0300

    Display delivery address in checkout summary

--- frontend/src/pages/checkout/checkout.jsx
+++ frontend/src/pages/checkout/checkout.jsx
@@ -1,22 +1,27 @@
 import { Summary } from "../components/Summary.jsx";
 import { Products } from "./components/products.jsx";
 import { MercadoPagoPayment } from "./components/mercadoPago/MercadoPagoPayment.jsx";
+import { useSelector } from "react-redux";
 
 function Checkout() {
   const orderId = new URLSearchParams(window.location.search).get("orderId");
+  const { address, isAddressSet } = useSelector((state) => state.delivery);
   return (
     <>
       <h2 className=" text-2xl font-semibold my-5 w-full text-center">Finalizar compra</h2>
-      <section className="bg-[#FFFFFF] rounded-md shadow-[0px_4px_8px_0px_rgba(0,_0,_0,_0.1)] mx-5 py-5">
-        <h2 className=" text-xl font-medium px-3 mb-10">Resumen del pedido</h2>
-
-        <section className="mx-5">
-          <Products />
-          <hr className="opacity-50 my-4" />
-          <Summary />
-          <MercadoPagoPayment orderId={orderId} />
-        </section>
+      <p className="text-sm w-full text-center mb-5 text-gray-700">Orden #{orderId.slice(0, 8)}</p>
+      <section className="bg-[#FFFFFF] rounded-md shadow-[0px_4px_8px_0px_rgba(0,_0,_0,_0.1)] mx-5 py-5 px-5">
+        <h2 className=" text-xl font-medium mb-5">Resumen del pedido</h2>
+        {isAddressSet && (
+          <p className="text-xs mb-5 opacity-90">
+            Enviar a: {address.street}, {address.houseNumber}
+          </p>
+        )}
+        <Products />
+        <hr className="opacity-50 my-4" />
+        <Summary />
       </section>
+      <MercadoPagoPayment orderId={orderId} />
     </>
   );
 }

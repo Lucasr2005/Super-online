47cab22f5db86114bfa39d38fea26b3491453730
Lucas Romero <lukisromero@hotmail.com.ar>
2026-02-10 11:09:24 -0300

    update order state using external_reference from payment detail

--- backend/controllers/orders.functions.js
+++ backend/controllers/orders.functions.js
@@ -61,7 +61,7 @@ export async function getShippingPrice(req, res) {
 }
 
 // SDK de Mercado Pago
-import { MercadoPagoConfig, Preference } from 'mercadopago';
+import { MercadoPagoConfig, Preference, Payment } from 'mercadopago';
 import pool from "../database/db.connection.js";
 
 const setItems = (products, cart, shippingPrice) => {
@@ -119,6 +119,43 @@ export async function createMPOrder(req, res) {
         console.error(error);
         res.status(500).send("Ha ocurrido un error interno al crear la orden");
     }
+}
+
+export async function receiveWebhook(req, res) {
+    const { body } = req;
+    if (body.type === "payment") {
+        const paymentId = body.data.id;
+
+        try {
+            const client = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN });
+            const payment = new Payment(client);
+
+            const paymentDetails = await payment.get({
+                id: paymentId,
+            });
+
+            const orderId = paymentDetails.external_reference;
+            const status = paymentDetails.status;
+
+            if (status === "approved") {
+                const approved = await pool.query("UPDATE orders SET state = $1 WHERE id = $2", [status, orderId]);
+                if (approved) {
+                    return res.status(200).json({ message: "El pago se ha aprobado." });
+                }
+            }
+            else if (status === "rejected") {
+                const declined = await pool.query("UPDATE orders SET state = $1 WHERE id = $2", [status, orderId]);
+                if (declined) {
+                    return res.status(200).json({ message: "El pago se ha rechazado." });
+                }
+            }
+
+        } catch (error) {
+            console.error("Error al obtener detalles del pago:", error);
+            return res.status(500).json({ message: "Error al procesar el webhook." });
+        }
+    }
+    res.sendStatus(200);
 }
 
 function validateOrderPayload({ cart, shippingPrice, address }) {

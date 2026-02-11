780ce4cd2533c6fb106032cf4c1da7356cb8340a
Lucas Romero <lukisromero@hotmail.com.ar>
2026-02-11 10:02:55 -0300

    Create main header layout

--- frontend/src/pages/components/header.jsx
+++ frontend/src/pages/components/header.jsx
@@ -1,3 +1,43 @@
+import { Link } from "react-router-dom";
+import cartImg from "../images/cart.png";
+import menuImg from "../images/menu.png";
+import searchImg from "../images/search.png";
+
 export function Header() {
-  return <header>header</header>;
+  return (
+    <header className="flex items-center py-4 shadow-[0_4px_4px_rgba(0,0,0,0.1)]">
+      <button
+        className="px-4"
+        aria-label="Abrir menú"
+      >
+        <img
+          src={menuImg}
+          alt="Icono de menú"
+          className="h-6"
+        />
+      </button>
+      <Link
+        to="/"
+        className="text-blue-800 font-semibold text-2xl flex flex-1"
+      >
+        Súper Online
+      </Link>
+      <div className="flex items-center mr-4 gap-4">
+        <button>
+          <img
+            src={searchImg}
+            alt="Icono de búsqueda"
+            className="h-8"
+          />
+        </button>
+        <Link to="/carrito">
+          <img
+            src={cartImg}
+            alt="Icono de carrito"
+            className="h-8"
+          />
+        </Link>
+      </div>
+    </header>
+  );
 }

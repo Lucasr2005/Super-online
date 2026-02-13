59ff0775f62a3dcf5a7394bd0128d632e2c97a7f
Lucas Romero <lukisromero@hotmail.com.ar>
2026-02-13 16:30:57 -0300

    Implement product sorting selection logic

--- frontend/src/pages/products/components/OrderBy.jsx
+++ frontend/src/pages/products/components/OrderBy.jsx
@@ -1,30 +1,39 @@
 import { OrderByOption } from "./OrderByOption";
+import { useDispatch } from "react-redux";
 
 export function OrderBy() {
+  const dispatch = useDispatch();
+  const handleChangeOption = (e) => {
+    dispatch({ type: "@filters/setOrderBy", payload: e.target.value });
+  };
   return (
     <div className="ml-2 mt-4 ">
-      <h3 className="text-lg font-semibold capitalize">Ordenar Por</h3>
+      <h3 className="text-lg font-semibold ">Ordenar por</h3>
       <section className="flex flex-col ml-4 ">
         <form>
           <OrderByOption
-            label="Menor precio"
+            label="Precio ascendente"
             name="order"
             value="priceAsc"
+            onChange={handleChangeOption}
           />
           <OrderByOption
-            label="Mayor precio"
+            label="Precio descendente"
             name="order"
             value="priceDesc"
+            onChange={handleChangeOption}
           />
           <OrderByOption
             label="Nombre ascendente"
             name="order"
             value="NameAsc"
+            onChange={handleChangeOption}
           />
           <OrderByOption
             label="Nombre descendente"
             name="order"
             value="NameDesc"
+            onChange={handleChangeOption}
           />
         </form>
       </section>

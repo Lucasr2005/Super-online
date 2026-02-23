1681362be54c4f2668d2db9066ba45e3b81c4275
Lucas Romero <lukisromero@hotmail.com.ar>
2026-02-23 14:14:51 -0300

    Implement endpoint to get user orders

---
+++ backend/controllers/orders/getOrdersByUserId.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+import pool from "../../database/db.connection.js";
+
+export async function getOrdersByUserId(req, res) {
+    const { auth_token } = req.cookies
+    if (!auth_token) {
+        return res.status(401).json({ message: "No se encontró token" });
+    }
+
+    try {
+        const decoded = jwt.verify(auth_token, process.env.JWT_SECRET);
+        const result = await pool.query("SELECT * FROM orders WHERE user_id=$1", [decoded.id]);
+        return res.status(200).json(result.rows);
+    } catch (error) {
+        if (error.name === 'JsonWebTokenError' || error.name === 'TokenExpiredError') {
+            return res.status(401).json({ message: "Token inválido" });
+        }
+        console.error("Error al verificar el token:", error);
+        return res.status(500).json({ message: "Error interno al verificar el token." });
+    }
+}

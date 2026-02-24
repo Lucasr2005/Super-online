e0143255555ecc1c3b886eaffe1aa71d6c0a9b6f
Lucas Romero <lukisromero@hotmail.com.ar>
2026-02-24 16:02:36 -0300

    Render home categories from global state via categoriesReducer

--- frontend/src/pages/home/components/category.jsx
+++ frontend/src/pages/home/components/category.jsx
@@ -1,18 +1,19 @@
 import { Link } from "react-router-dom";
 import { normalizeString } from "../../../functions/normalizeString.js";
 
-export function Category({ category, count, icon: Icon }) {
+export function Category({ category, count, imgName }) {
+  const imgURL = import.meta.env.VITE_BACKEND_API_URL + "/category/" + imgName;
+  console.log(imgURL);
   return (
     <Link
       to={`/productos/${normalizeString(category)}`}
-      className="group bg-card rounded-xl border border-border hover:border-primary hover:shadow-lg transition-all duration-300 flex flex-col items-center justify-center text-center aspect-square w-[100px]"
+      className="group bg-card rounded-xl border border-border hover:border-primary hover:shadow-lg transition-all duration-300 flex flex-col items-center justify-center text-center aspect-square w-[100px] gap-1"
     >
-      {Icon && (
-        <Icon
-          className="w-8 h-8 mb-3 text-primary transition-transform duration-300 group-hover:scale-110"
-          strokeWidth={1.5}
-        />
-      )}
+      <img
+        className="w-9 h-9 text-primary transition-transform duration-300 group-hover:scale-110  "
+        src={imgURL}
+        alt=""
+      />
       <h3 className="font-semibold text-foreground text-sm leading-tight">{category}</h3>
       <p className="text-xs text-muted-foreground mt-1">
         {count} {count === 1 ? "producto" : "productos"}

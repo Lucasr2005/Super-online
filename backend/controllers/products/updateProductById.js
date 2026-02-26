d802f5ce60372f5dcc3f0b956e105c8321ac18d9
Lucas Romero <lukisromero@hotmail.com.ar>
2026-02-26 10:33:32 -0300

    Rename image_url to img_name in product CRUD functions

--- backend/controllers/products/updateProductById.js
+++ backend/controllers/products/updateProductById.js
@@ -2,12 +2,12 @@ import pool from "../../database/db.connection.js"
 
 export async function updateProductById(req, res) {
     const { id } = req.params
-    const { name, category, brand, price, stock, image_url, sub_category } = req.body
-    if (!name || !category || !brand || !price || !stock || !image_url || !sub_category) {
+    const { name, category, brand, price, stock, img_name, sub_category } = req.body
+    if (!name || !category || !brand || !price || !stock || !img_name || !sub_category) {
         return res.status(400).json({ message: "Datos inclompletos" })
     }
     try {
-        const result = await pool.query("UPDATE products SET name=$1, category=$2, brand=$3, price=$4, stock=$5, image_url=$6, sub_category=$7 WHERE id=$8 RETURNING *", [name, category, brand, price, stock, image_url, sub_category, id])
+        const result = await pool.query("UPDATE products SET name=$1, category=$2, brand=$3, price=$4, stock=$5, img_name=$6, sub_category=$7 WHERE id=$8 RETURNING *", [name, category, brand, price, stock, img_name, sub_category, id])
         if (result.rows.length === 0) {
             return res.status(400).json({ message: "No se ha podido actualizar el producto" })
         }

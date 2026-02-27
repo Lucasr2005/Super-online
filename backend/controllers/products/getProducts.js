d2fbcb7bf8499ba6ff0f0c53adfa75fc28e59e5c
Lucas Romero <lukisromero@hotmail.com.ar>
2026-02-27 09:07:28 -0300

    Add products to cache

--- backend/controllers/products/getProducts.js
+++ backend/controllers/products/getProducts.js
@@ -1,13 +1,21 @@
 import pool from "../../database/db.connection.js"
+import { cache } from "../../cache.js"
 
 export async function getProducts(req, res) {
     try {
-        const result = await pool.query("SELECT p.*,c.name AS category FROM products p INNER JOIN categories c ON p.category_id = c.id")
-        if (result.rows.length === 0) {
-            return res.status(404).json({ message: "No se han encontrado productos" })
+        if (cache.has("products")) {
+            const products = cache.get("products")
+            return res.status(200).json(products)
         }
-        const products = result.rows
-        return res.status(200).json(products)
+        else {
+            const result = await pool.query("SELECT p.*,c.name AS category FROM products p INNER JOIN categories c ON p.category_id = c.id")
+            if (result.rows.length === 0) {
+                return res.status(404).json({ message: "No se han encontrado productos" })
+            }
+            const products = result.rows
+            cache.set("products", products)
+            return res.status(200).json(products)
+        }
 
     } catch (error) {
         console.log(error)

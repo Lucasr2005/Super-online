77e8f73793d813868070ff461aa856faf6721e25
Lucas Romero <lukisromero@hotmail.com.ar>
2026-02-27 09:26:10 -0300

    Add products images to cache

--- backend/controllers/products/getProductImg.js
+++ backend/controllers/products/getProductImg.js
@@ -1,7 +1,13 @@
 import path from "path";
+import { cache } from "../../cache.js"
+
 export async function getProductImg(req, res) {
     const { fileName } = req.params;
     try {
+        if (cache.has(`product_img_${fileName}`)) {
+            const cachedFilePath = cache.get(`product_img_${fileName}`);
+            return res.sendFile(cachedFilePath);
+        }
         const filePath = path.join(process.cwd(), "public", "assets", "products", fileName);
         res.sendFile(filePath);
     } catch (error) {

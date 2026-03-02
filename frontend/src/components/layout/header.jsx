3d52869dec848c98239a174635079cf5e9ece5fc
Lucas Romero <lukisromero@hotmail.com.ar>
2026-03-02 09:06:17 -0300

    Replace header PNG icons with Lucide components

--- frontend/src/components/layout/header.jsx
+++ frontend/src/components/layout/header.jsx
@@ -1,22 +1,20 @@
 import { Link } from "react-router-dom";
-import cartImg from "../../assets/images/cart.png";
-import menuImg from "../../assets/images/menu.png";
 import { handleSetSidebar } from "./functions/setSidebar.js";
 import { useDispatch } from "react-redux";
+import { Menu, ShoppingCart } from "lucide-react";
 
 export function Header() {
   const dispatch = useDispatch();
   return (
     <header className="flex items-center py-4 shadow-[0_4px_4px_rgba(0,0,0,0.1)] h-16 sticky top-0 z-50 mt-0 w-full bg-[#fafafa]">
       <button
-        className="px-4"
         aria-label="Abrir menú"
         onClick={() => handleSetSidebar({ dispatch })}
       >
-        <img
-          src={menuImg}
-          alt="Icono de menú"
-          className="h-6"
+        <Menu
+          className="h-8 mx-4"
+          width={28}
+          height={28}
         />
       </button>
       <Link
@@ -26,10 +24,10 @@ export function Header() {
         Súper Online
       </Link>
       <Link to="/carrito">
-        <img
-          src={cartImg}
-          alt="Icono de carrito"
+        <ShoppingCart
           className="h-8 mx-4"
+          width={28}
+          height={28}
         />
       </Link>
     </header>

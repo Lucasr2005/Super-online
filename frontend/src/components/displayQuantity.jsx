706e9b2c53cb5a204a5ba03277c0492bade6af06
Lucas Romero <lukisromero@hotmail.com.ar>
2026-03-04 08:54:03 -0300

    Add confirmation modal before removing product from cart

--- frontend/src/components/displayQuantity.jsx
+++ frontend/src/components/displayQuantity.jsx
@@ -1,8 +1,11 @@
 import { useDispatch } from "react-redux";
 import { Plus, Minus } from "lucide-react";
 import { toast } from "react-hot-toast";
+import { RemoveCartItem } from "./removeCartItem.jsx";
+import { useState } from "react";
 
 export function DisplayQuantity({ quantity, id }) {
+  const [displayRemove, setDisplayRemove] = useState(false);
   const dispatch = useDispatch();
   const incrementQuantity = (id) => {
     if (quantity >= 10) {
@@ -13,13 +16,19 @@ export function DisplayQuantity({ quantity, id }) {
   };
   const decreaseQuantity = (id) => {
     if (quantity === 1) {
-      const ok = window.confirm("Desea eliminar el producto del carrito?");
-      if (!ok) return;
+      setDisplayRemove(true);
+      return;
     }
     dispatch({ type: "@cart/decreaseQuantity", payload: { id } });
   };
   return (
     <>
+      {displayRemove && (
+        <RemoveCartItem
+          productId={id}
+          display={setDisplayRemove}
+        />
+      )}
       <div className="rounded-lg flex items-center">
         <button
           className="px-2  text-2xl cursor-pointer rounded-full"

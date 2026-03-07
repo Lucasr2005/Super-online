c3ffd7566bffcedd2a2fa553aad571adf35ac7a0
Lucas Romero <lukisromero@hotmail.com.ar>
2026-03-07 14:27:43 -0300

    Remove image controllers for categories and products; adjust Vercel paths and configuration to serve static files

--- backend/routes/products.routes.js
+++ backend/routes/products.routes.js
@@ -4,7 +4,6 @@ import { getProducts } from "../controllers/products/getProducts.js"
 import { getProductById } from "../controllers/products/getProductById.js"
 import { updateProductById } from "../controllers/products/updateProductById.js"
 import { deleteProductById } from "../controllers/products/deleteProductById.js"
-import { getProductImg } from "../controllers/products/getProductImg.js"
 const router = express.Router()
 
 router.post("/products", createProduct)
@@ -13,6 +12,5 @@ router.get("/products/:id", getProductById)
 router.put("/products/:id", updateProductById)
 router.delete("/products/:id", deleteProductById)
 
-router.get("/public/products/:fileName", getProductImg)
 
 export default router
